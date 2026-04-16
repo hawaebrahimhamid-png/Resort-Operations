a2cf07ae05c2495b68dc6d7c1c72f1bcb6d8effc
hawa ebrahim <hawaebrahimhamid@gmail.com>
2026-04-16 20:58:00 +0800

    autheraztion fix

--- resort-ai-backend/middleware/auth.js
+++ resort-ai-backend/middleware/auth.js
@@ -1,20 +1,28 @@
 import jwt from "jsonwebtoken";
 
-// ✅ AUTH (check token)
 export const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization;
+   console.log("SECRET:", process.env.JWT_SECRET);
+    const authHeader = req.headers.authorization;
 
-  if (!token) return res.status(401).json({ error: "No token" });
+  if (!authHeader) {
+    return res.status(401).json({ error: "No token" });
+  }
+
+  // ✅ Check format
+  if (!authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Invalid token format" });
+  }
+
+  const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // contains id + role
+    req.user = decoded;
     next();
-  } catch {
+  } catch (error) {
     res.status(401).json({ error: "Invalid token" });
   }
 };
-
 // ✅ ROLE CHECK (manager only)
 export const isManager = (req, res, next) => {
   if (req.user.role !== "manager") {
